ce5bfbcf4006d3b7738f56d4cd4d9913ff20c393
Conor McGrath <conor909@gmail.com>
2016-10-14 17:21:04 +0100

    select elements after render

--- src/clojure_proj/canvas.cljs
+++ src/clojure_proj/canvas.cljs
@@ -1,30 +1,25 @@
 (ns clojure-proj.canvas)
 
-(def canvas (.getElementById js/document "my-canvas"))
-(def ctx (.getContext canvas "2d"))
-
 (defn fill-style [ctx color]
   (set! (.-fillStyle ctx) color)
-)
+  )
+(defn init-canvas [el]
+  (set! (.-width el) (.-innerWidth js/window))
+  (set! (.-height el) (.-innerWidth js/window)))
 
-(defn init-canvas []
-  (set! (.-width canvas) (.-innerWidth js/window) )
-  (set! (.-height canvas) (.-innerWidth js/window)))
-
-(defn draw-drop [x y]
+(defn draw-drop [ctx x y]
+  (.save ctx)
+  ;(.translate ctx x y)
   (.beginPath ctx)
-  (.moveTo ctx  (+ x 100) y)
+  (.moveTo ctx (+ x 100) y)
   (.lineTo ctx (+ x 145) (+ y 75))
   (.arc ctx (+ x 100) (+ y 75) 45 0 (* 1 Math/PI))
-  (.moveTo ctx  (+ x 55) (+ x 75))
+  (.moveTo ctx (+ x 55) (+ x 75))
   (.lineTo ctx (+ x 100) y)
   (fill-style ctx "rgba(255, 255, 255, 0.5)")
   (.fill ctx)
-)
+  (.restore ctx))
 
-(defn make-it-raaiiin [x y]
-  (loop [x 1000]
-    (when (> x 1)
-      (draw-drop x y)
-      (recur (- x 100))))
-)
+(defn make-it-raaiiin [ctx y]
+  (dotimes [x 1000]
+    (draw-drop ctx x y)))
--- src/clojure_proj/core.cljs
+++ src/clojure_proj/core.cljs
@@ -6,37 +6,37 @@
 
 (defn header-component [props]
   (let [subHead (:subHead props) head (:head props)]
-       [:div  {:style {:textAlign "center" :fontFamily "'Schoolbell', cursive" :color "#fff" :textShadow "0px 0px 10px #fff"}}
-       [:div  {:style {:fontSize "2em"}} subHead]
-       [:div  {:style {:fontSize "3em"}} head]]))
+    [:div {:style {:textAlign "center" :fontFamily "'Schoolbell', cursive" :color "#fff" :textShadow "0px 0px 10px #fff"}}
+     [:div {:style {:fontSize "2em"}} subHead]
+     [:div {:style {:fontSize "3em"}} head]]))
 
 (defn canvas-el []
-  [:canvas {:id "my-canvas" :style {:position "absolute"}} ])
+  [:canvas {:id "my-canvas" :style {:position "absolute"}}])
+
+(def night-sky-gradient
+  ["linear-gradient(180deg, #152b52 7%, #2c9ab3 51%, #f3f3d1 90%", "#f3f3d1"])
 
 (defn app []
-  [:div {
-       :style {
-         :position "relative"
-         :height "100%"
-         :background ["linear-gradient(180deg, #152b52 7%, #2c9ab3 51%, #f3f3d1 90%", "#f3f3d1"]
-       }
-    }
-    [canvas-el]
-    [canvas/init-canvas]
-    [header-component {:subHead "Welcome to" :head "Rose Cottage"}]
-   ]
-)
+  [:div
+   {:style
+    {:position   "relative"
+     :height     "100%"
+     :background night-sky-gradient}}
+   [canvas-el]
+   [header-component {:subHead "Welcome to" :head "Rose Cottage"}]])
 
-(def environment {:time "It is currently day time"
+(def environment {:time    "It is currently day time"
                   :weather "It is currently raining"})
 
+(defn render-canvas []
+  (let [el (.getElementById js/document "my-canvas")
+        ctx (.getContext el "2d")]
+    (canvas/init-canvas el)
+    (when (= (get environment :weather) "It is currently raining")
+      (canvas/make-it-raaiiin ctx 0 0))))
+
 (defn run []
-  (reagent/render [app] (.getElementById js/document "app-container"))
-    (canvas/init-canvas)
-    (if (= (get environment :weather) "It is currently raining")
-        (canvas/make-it-raaiiin 0 0)
-    )
-)
+  (reagent/render [app] (.getElementById js/document "app-container") render-canvas))
 
 (run)
 
